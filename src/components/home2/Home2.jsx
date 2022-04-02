a4daae8c47d40f9afb80bc9c9b6621c1c47f712a
Kbejj <98552881+Kbejj@users.noreply.github.com>
2022-04-02 19:59:52 +0300

    Update Home2.jsx

--- src/components/home2/Home2.jsx
+++ src/components/home2/Home2.jsx
@@ -13,11 +13,10 @@ const Home2 = () => {
         </div>
 
         <div className='about2__right'>
-          <h3> <span> Virtuarld </span>aims to solve 3 main problems, </h3>
+          <h3> <span> Virtuarld </span>aims to solve 2 main problems, </h3>
           <ul>
             <h4>
-              <li> • World Hunger: users will earn money by taking care of themselves in both The Real World and in Virtuarld;</li>
-              <li>• Distanced Relations: Virtuarld will enhance the online interaction experience with your friends from all over the world; </li>
+              <li>• Distant Relations: Virtuarld will enhance the online interaction experience with your friends from all over the world; </li>
               <li>• Remote Jobs and Education: Virtuarld is aiming to solve the major problems the world faced in remote jobs and education during the pandemic. </li>
               <li>Virtuarld will not only solve these problems, but will change the way we interact with social media, websites and movie streaming, taking the customer's experience to the next level.</li>
             </h4>
